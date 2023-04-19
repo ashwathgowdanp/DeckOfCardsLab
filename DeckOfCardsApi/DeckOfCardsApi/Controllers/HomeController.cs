46b44014f6f334e27f0bd458c34851d0c63d31ce
ashwathgowdanp <ashwathgowdanp@gmail.com>
2023-04-19 06:32:41 -0400

    deck of card iniital commit

---
+++ DeckOfCardsApi/DeckOfCardsApi/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using DeckOfCardsApi.Models;
+using DeckOfCardsApi.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeckOfCardsApi.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly CardsApiService cardsApiService;
+
+        public HomeController(CardsApiService cardsApiService)
+        {
+            this.cardsApiService = cardsApiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                List<CardShuffle> result = await cardsApiService.GetShuffledCards(1);
+                return View(result);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Exception = ex;
+                Debug.WriteLine(ex);
+                return View();
+            }
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckOfCardsApi/DeckOfCardsApi/Services/CardsApiService.cs
@@ -0,0 +1,31 @@
+﻿using DeckOfCardsApi.Models;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace DeckOfCardsApi.Services
+{
+    public class CardsApiService
+    {
+        private readonly HttpClient _httpClient;
+        public CardsApiService(HttpClient httpClient) 
+        {
+            _httpClient = httpClient;
+        }
+
+        //https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
+        public async Task<List<CardShuffle>> GetShuffledCards(int count)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"new/shuffle/?deck_count={count}");
+         // dynamic  List<CardShuffle> cards = await response.Content.ReadAsAsync<List<CardShuffle>>();
+            var result = await response.Content.ReadAsStringAsync();
+            List<CardShuffle> cards = JsonConvert.DeserializeObject<List<CardShuffle>>(result);
+            
+            // Console.WriteLine( result );
+
+            return cards;
+            
+        }
+    }
+}
